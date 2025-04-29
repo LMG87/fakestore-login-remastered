4ab4512326bf46541ae37729b4f75539c79b49a3
LMG87 <lm.gomez.guerrero.87@gmail.com>
2025-04-29 11:58:24 -0500

    feat: Implement product and user card components with styling
    - Added card-product component for displaying product details including image, title, description, and buttons for viewing details and adding to cart.
    - Introduced card-user component to show user information such as name, email, and address.
    - Created cart-product component to manage items in the cart with functionality to increase, decrease, or remove items.
    - Developed login component with form validation and user authentication.
    - Implemented cart component to display items in the cart and calculate total price.
    - Added product detail component to show detailed information about a selected product.
    - Created product list component to display a list of products fetched from the API.
    - Established user component to list all users with their details.
    - Integrated services for authentication, product management, user management, and cart state management.
    - Added routing for product and user pages.
    - Styled components with CSS for better UI/UX.
    - Implemented local storage for cart persistence.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Products',
+    canActivate: [authGuard],
+    loadChildren: () => import('./pages/products/product.route'),
+  },
+  {
+    path: 'users',
+    title: 'Users',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/users/users.component'),
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    loadComponent: () => import('./pages/auth/login/login.component'),
+  },
+  {
+    path: 'cart',
+    title: 'Cart',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/cart/cart.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+];
